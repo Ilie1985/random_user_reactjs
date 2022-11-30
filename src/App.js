bd022b261ecdfc349d3925653fef79b514456327
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-30 10:34:26 +0000

    display default image and containers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import {
   FaEnvelopeOpen,
   FaUser,
@@ -6,11 +6,48 @@ import {
   FaMap,
   FaPhone,
   FaLock,
-} from 'react-icons/fa'
-const url = 'https://randomuser.me/api/'
-const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+} from "react-icons/fa";
+const url = "https://randomuser.me/api/";
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+
 function App() {
-  return <h2>random user starter</h2>
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState("name");
+  const [value, setValue] = useState("random person");
+
+  const handleValue = (e) => {
+    console.log(e.target.value);
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+
+    if (loading) {
+      return <h1> Loading...</h1>;
+    }
+
+    const response = await fetch(url);
+    const data = await response.json();
+    setLoading(false);
+    setPerson(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <div className="block bcg-black"></div>
+
+      <div className="block">
+        <div className="container">
+          <img src={defaultImage} alt="random user" className="user-img" />
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
